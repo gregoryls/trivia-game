4af8491fd7bbee4ec3b53154a711315e84e99d85
Greg Schneider <greg53x@msn.com>
2023-02-09 23:08:47 -0700

    separate the first row of boxes with a different class from the other questions to make a header row

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
 //TODO dynamically create 5 columns of 6 rows of boxes
 //to be clicked on and open a modal with the question
+//TODO create player iife
 
 const triviaBoard = (() => {
     let gridArea = document.querySelector('#gridWrapper');
     
     console.log(gridArea)
     const createQuestionGrid = (questionTotal) =>{
-        let array = [1,2,3,4,5,6,7,8,9,10]
+        //point values for rows 1-5 on the question grid. 
+        let questionValues = [100,100,100,100,100,100,
+                              200,200,200,200,200,200,
+                              300,300,300,300,300,300,
+                              400,400,400,400,400,400,
+                              500,500,500,500,500,500]
         for (let i=0;i<questionTotal;i++){
+            // for (i;i<6;i++){
+            //     console.log(i);
+            // }
             let questionDiv = document.createElement('div');
-            questionDiv.classList.add('question');
-            questionDiv.textContent = array[i];
+            //treat the first six boxes differently to turn them into topic headers. 
+            if (i<6) {
+                questionDiv.classList.add('questionTopic');
+                questionDiv.textContent = 'topic';
+            } else {
+                questionDiv.classList.add('question');
+                questionDiv.textContent = questionValues[i-6];
+            }
+            
             gridArea.appendChild(questionDiv);
         }
     }
