81ba1fb063d743d7fb74fa694e967c680d855f82
Greg Schneider <greg53x@msn.com>
2023-02-11 23:10:16 -0700

    add a modal template to be modified

--- index.js
+++ index.js
@@ -37,6 +37,35 @@ const triviaBoard = (() => {
     }
 })();
 
+
+const questionModal = (()=>{
+    //create a modal that contains general help information on playing the game
+    // TODO convert button to question square div
+    // const openModalButton = document.querySelector('#helpModal');
+    const modal = document.querySelector('.modal');
+    const overlay = document.querySelector('.overlay');
+
+    const removeClass = (element,className) =>{
+        element.classList.remove(className);
+    }
+
+    const addClass = (element,className) =>{
+        element.classList.add(className);
+    }
+    //overlay provides a semi-transparent blur behind the modal to help focus attention on modal
+    //when the overlay background is clicked anywhere, the modal regains the hidden class and disappears
+    overlay.addEventListener('click', ()=>{
+        addClass(modal,'hidden');
+        addClass(overlay,'hidden');
+    });
+
+    // TODO convert this into working when a question square is clicked
+    // openModalButton.addEventListener('click',()=> {
+    //     removeClass(modal,'hidden');
+    //     removeClass(overlay,'hidden');
+    // });
+})();
+
 triviaBoard.createQuestionGrid(36)
 
 document.querySelector('h1').innerHTML = '<img src=\'/img/Lunar-Eclipse-Leona.webp\'>';
