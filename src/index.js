53bb42a4c4d7656d5dfc3837782cdeef1368450d
Greg Schneider <greg53x@msn.com>
2023-08-26 23:14:08 -0600

    initialize question modal iife before calling papaparse

--- src/index.js
+++ src/index.js
@@ -14,6 +14,111 @@ import Papa from "papaparse";
 const fileInput = document.getElementById("fileInput");
 const upload = document.getElementById("upload");
 
+const questionModal = (() => {
+  //create a modal that contains a question based on the clicked question tile
+  // TODO convert button to question square div
+  // const openModalButton = document.querySelector('#helpModal');
+  let currentScore = 0;
+  const modal = document.querySelector(".modal");
+  const overlay = document.querySelector(".overlay");
+
+  const removeClass = (element, className) => {
+    element.classList.remove(className);
+  };
+
+  const addClass = (element, className) => {
+    element.classList.add(className);
+  };
+
+  //overlay provides a semi-transparent blur behind the modal to help focus attention on modal
+  //when the overlay background is clicked anywhere, the modal regains the hidden class and disappears
+  overlay.addEventListener("click", () => {
+    addClass(modal, "hidden");
+    addClass(overlay, "hidden");
+  });
+
+  // TODO convert this into working when a question square is clicked
+  const addModalEventListener = () => {
+    let questions = document.querySelectorAll(".question");
+    console.log(questions);
+
+    for (let i = 0; i < questions.length; i++) {
+      questions[i].addEventListener("click", () => {
+        //prevent tile from being opened twice
+        if (questions[i].classList.contains("taken")) return;
+        //associated array numbers are due to the format of question csv input
+        if (i < 6) modal.innerHTML = getQuestionData.questions[2 * i][1];
+        if (i < 12 && i > 5)
+          modal.innerHTML = getQuestionData.questions[2 * i - 12][2];
+        if (i < 18 && i > 11)
+          modal.innerHTML = getQuestionData.questions[2 * i - 24][3];
+        if (i < 24 && i > 17)
+          modal.innerHTML = getQuestionData.questions[2 * i - 36][4];
+        if (i < 30 && i > 23)
+          modal.innerHTML = getQuestionData.questions[2 * i - 48][5];
+        if (i < 36 && i > 29)
+          modal.innerHTML = getQuestionData.questions[2 * i - 60][6];
+
+        currentScore = parseInt(questions[i].textContent, 10);
+        console.log(currentScore);
+
+        //add an answer button to the modal that reveals the answer when clicked, button
+        //adds before the user buttons, answer adds to bottom of modal
+        const btn = document.createElement("button");
+        btn.textContent = "Answer";
+        btn.addEventListener("click", () => {
+          //associated array numbers are due to the format of question csv input
+          if (i < 6) modal.innerHTML += getQuestionData.questions[2 * i + 1][1];
+          if (i < 12 && i > 5)
+            modal.innerHTML += getQuestionData.questions[2 * i - 11][2];
+          if (i < 18 && i > 11)
+            modal.innerHTML += getQuestionData.questions[2 * i - 23][3];
+          if (i < 24 && i > 17)
+            modal.innerHTML += getQuestionData.questions[2 * i - 35][4];
+          if (i < 30 && i > 23)
+            modal.innerHTML += getQuestionData.questions[2 * i - 47][5];
+          if (i < 36 && i > 29)
+            modal.innerHTML += getQuestionData.questions[2 * i - 59][6];
+
+          let div = document.createElement("div");
+          div.classList.add("scoreButtons");
+          for (let j = 0; j < 4; j++) {
+            //set loop length to number of players
+            //add buttons to the modal that will award points to the correct player(s)
+            const btn = document.createElement("button");
+            btn.textContent = playerNames[j];
+            btn.addEventListener("click", () => {
+              //checks for clicked class added later to prevent awarding points more than once
+              if (btn.classList.contains("clicked")) return;
+              //get question value from the clicked tile
+              currentScore = parseInt(questions[i].textContent, 10);
+              //add current score to the appropriate player when clicked
+              j === 0
+                ? player1.displayPlayerScore(1, currentScore)
+                : j === 1
+                ? player2.displayPlayerScore(2, currentScore)
+                : j === 2
+                ? player3.displayPlayerScore(3, currentScore)
+                : player4.displayPlayerScore(4, currentScore);
+              btn.classList.add("clicked");
+            });
+            div.appendChild(btn);
+          }
+          modal.appendChild(div);
+        });
+
+        modal.appendChild(btn);
+
+        addClass(questions[i], "taken");
+
+        removeClass(modal, "hidden");
+        removeClass(overlay, "hidden");
+      });
+    }
+  };
+  return { addModalEventListener, currentScore };
+})();
+
 upload.addEventListener("click", () => {
   // getQuestionData.csvParse(fileInput);
   getQuestionData.csvParse();
@@ -133,111 +238,6 @@ const playerSetup = (() => {
   document.querySelector("#player4").addEventListener("click", () => {
     userName(player4);
   });
-})();
-
-const questionModal = (() => {
-  //create a modal that contains a question based on the clicked question tile
-  // TODO convert button to question square div
-  // const openModalButton = document.querySelector('#helpModal');
-  let currentScore = 0;
-  const modal = document.querySelector(".modal");
-  const overlay = document.querySelector(".overlay");
-
-  const removeClass = (element, className) => {
-    element.classList.remove(className);
-  };
-
-  const addClass = (element, className) => {
-    element.classList.add(className);
-  };
-
-  //overlay provides a semi-transparent blur behind the modal to help focus attention on modal
-  //when the overlay background is clicked anywhere, the modal regains the hidden class and disappears
-  overlay.addEventListener("click", () => {
-    addClass(modal, "hidden");
-    addClass(overlay, "hidden");
-  });
-
-  // TODO convert this into working when a question square is clicked
-  const addModalEventListener = () => {
-    let questions = document.querySelectorAll(".question");
-    console.log(questions);
-
-    for (let i = 0; i < questions.length; i++) {
-      questions[i].addEventListener("click", () => {
-        //prevent tile from being opened twice
-        if (questions[i].classList.contains("taken")) return;
-        //associated array numbers are due to the format of question csv input
-        if (i < 6) modal.innerHTML = getQuestionData.questions[2 * i][1];
-        if (i < 12 && i > 5)
-          modal.innerHTML = getQuestionData.questions[2 * i - 12][2];
-        if (i < 18 && i > 11)
-          modal.innerHTML = getQuestionData.questions[2 * i - 24][3];
-        if (i < 24 && i > 17)
-          modal.innerHTML = getQuestionData.questions[2 * i - 36][4];
-        if (i < 30 && i > 23)
-          modal.innerHTML = getQuestionData.questions[2 * i - 48][5];
-        if (i < 36 && i > 29)
-          modal.innerHTML = getQuestionData.questions[2 * i - 60][6];
-
-        currentScore = parseInt(questions[i].textContent, 10);
-        console.log(currentScore);
-
-        //add an answer button to the modal that reveals the answer when clicked, button
-        //adds before the user buttons, answer adds to bottom of modal
-        const btn = document.createElement("button");
-        btn.textContent = "Answer";
-        btn.addEventListener("click", () => {
-          //associated array numbers are due to the format of question csv input
-          if (i < 6) modal.innerHTML += getQuestionData.questions[2 * i + 1][1];
-          if (i < 12 && i > 5)
-            modal.innerHTML += getQuestionData.questions[2 * i - 11][2];
-          if (i < 18 && i > 11)
-            modal.innerHTML += getQuestionData.questions[2 * i - 23][3];
-          if (i < 24 && i > 17)
-            modal.innerHTML += getQuestionData.questions[2 * i - 35][4];
-          if (i < 30 && i > 23)
-            modal.innerHTML += getQuestionData.questions[2 * i - 47][5];
-          if (i < 36 && i > 29)
-            modal.innerHTML += getQuestionData.questions[2 * i - 59][6];
-
-          let div = document.createElement("div");
-          div.classList.add("scoreButtons");
-          for (let j = 0; j < 4; j++) {
-            //set loop length to number of players
-            //add buttons to the modal that will award points to the correct player(s)
-            const btn = document.createElement("button");
-            btn.textContent = playerNames[j];
-            btn.addEventListener("click", () => {
-              //checks for clicked class added later to prevent awarding points more than once
-              if (btn.classList.contains("clicked")) return;
-              //get question value from the clicked tile
-              currentScore = parseInt(questions[i].textContent, 10);
-              //add current score to the appropriate player when clicked
-              j === 0
-                ? player1.displayPlayerScore(1, currentScore)
-                : j === 1
-                ? player2.displayPlayerScore(2, currentScore)
-                : j === 2
-                ? player3.displayPlayerScore(3, currentScore)
-                : player4.displayPlayerScore(4, currentScore);
-              btn.classList.add("clicked");
-            });
-            div.appendChild(btn);
-          }
-          modal.appendChild(div);
-        });
-
-        modal.appendChild(btn);
-
-        addClass(questions[i], "taken");
-
-        removeClass(modal, "hidden");
-        removeClass(overlay, "hidden");
-      });
-    }
-  };
-  return { addModalEventListener, currentScore };
 })();
 
 // triviaBoard.createQuestionGrid(36);
