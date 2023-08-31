30fa3321baa3ceabecee5ea659a1ae6323631660
Greg Schneider <greg53x@msn.com>
2023-08-30 22:59:12 -0600

    delete old player generation function from index

--- src/index.js
+++ src/index.js
@@ -164,24 +164,6 @@ const getQuestionData = (() => {
   return { questions, csvParse };
 })();
 
-const player = (name, number) => {
-  const displayPlayerName = function (playerNumber) {
-    document.querySelector(
-      `#player${playerNumber}`,
-    ).textContent = `${this.name}:`;
-  };
-  const displayPlayerScore = function (playerNumber, scoreToAdd) {
-    // thank about splitting this function into two functions
-    this.score += scoreToAdd;
-    document.querySelector(`#player${playerNumber}Score`).textContent =
-      this.score;
-  };
-  this.name = name;
-  this.score = 0;
-  this.number = number;
-  return { name, number, score, displayPlayerName, displayPlayerScore };
-};
-
 const playerSetup = (() => {
   // display default usernames
   player1.displayPlayerName(1);
