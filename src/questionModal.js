7e26dd5830655cb28099bd4f52673e0e982fd575
Greg Schneider <greg53x@msn.com>
2023-09-01 22:33:27 -0600

    update question box color

--- src/questionModal.js
+++ src/questionModal.js
@@ -15,7 +15,7 @@ export function addClass(element, className) {
   element.classList.add(className);
 }
 
-export function addQuestionModalEventListener() {
+export function addQuestionModalEventListener(playerNamesArray) {
   const questions = document.querySelectorAll(".question");
   console.log(questions);
 
@@ -53,7 +53,7 @@ export function addQuestionModalEventListener() {
           // set loop length to number of players
           // add buttons to the modal that will award points to the correct player(s)
           const btn = document.createElement("button");
-          btn.textContent = playerNames[j];
+          btn.textContent = playerNamesArray[j];
           btn.addEventListener("click", () => {
             // checks for clicked class added later to prevent awarding points more than once
             if (btn.classList.contains("clicked")) return;
