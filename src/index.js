2e1a11fa617796232b08c5bd66246c0260fcc804
Greg Schneider <greg53x@msn.com>
2023-09-01 21:36:56 -0600

    delete old player setup iife in favor of new module

--- src/index.js
+++ src/index.js
@@ -166,40 +166,6 @@ const getQuestionData = (() => {
   return { questions, csvParse };
 })();
 
-// const playerSetup = (() => {
-//   // display default usernames
-//   player1.displayPlayerName(1);
-//   player2.displayPlayerName(2);
-//   player3.displayPlayerName(3);
-//   player4.displayPlayerName(4);
-//   // display intial (zero) player scores
-//   player1.displayPlayerScore(1, 0);
-//   player2.displayPlayerScore(2, 0);
-//   player3.displayPlayerScore(3, 0);
-//   player4.displayPlayerScore(4, 0);
-//   // allow players to change their displayed name
-//   const userName = (player) => {
-//     const tempName = player.name;
-//     player.name = prompt("Please Enter your name", "Josh Gunson");
-//     // prevent player name from disappearing by using previous name
-//     if (player.name === null) player.name = tempName;
-//     player.displayPlayerName(player.number);
-//   };
-//   // player names can be clicked to open the change dialogue
-//   document.querySelector("#player1").addEventListener("click", () => {
-//     userName(player1);
-//   });
-//   document.querySelector("#player2").addEventListener("click", () => {
-//     userName(player2);
-//   });
-//   document.querySelector("#player3").addEventListener("click", () => {
-//     userName(player3);
-//   });
-//   document.querySelector("#player4").addEventListener("click", () => {
-//     userName(player4);
-//   });
-// })();
-
 // triviaBoard.createQuestionGrid(36);
 
 // document.querySelector('h1').innerHTML = '<img src=\'/img/Lunar-Eclipse-Leona.webp\'>';
