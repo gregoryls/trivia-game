5d03cc5ccaf676338b09d29c1c54eda5ad9b76ed
Greg Schneider <greg53x@msn.com>
2023-09-25 13:14:30 -0600

    change board generation loop to start with dedicated category tile loop

--- src/board.js
+++ src/board.js
@@ -10,23 +10,28 @@ export function createQuestionGrid(gridCount, questionObject) {
     300, 300, 300, 400, 400, 400, 400, 400, 400, 500, 500, 500, 500, 500, 500,
     600, 600, 600, 600, 600, 600,
   ];
+  const categoryCount = Object.keys(questionObject).length;
+  console.log(categoryCount);
 
-  for (let i = 0; i < gridCount; i += 1) {
+  for (let i = 0; i < categoryCount; i += 1) {
     const questionDiv = document.createElement("div");
     // treat the first six boxes differently to turn them into topic headers.
-    if (i < 6) {
-      // 00,20,40,60,80,100
-      questionDiv.classList.add("questionTopic");
-      // use every second array to account for spreadsheet format
-      questionDiv.textContent = questionObject[`category${i}`].topic;
+    // if (i < 6) {
+    //   // 00,20,40,60,80,100
+    //   questionDiv.classList.add("questionTopic");
+    //   // use every second array to account for spreadsheet format
+    //   questionDiv.textContent = questionObject[`category${i}`].topic;
 
-      // const [textContent] = questionObject[2 * i];
-      // questionDiv.textContent = textContent;
-      // questionDiv.textContent = questionData[2 * i][0];
-    } else {
-      questionDiv.classList.add("question");
-      questionDiv.textContent = questionValues[i - 6];
-    }
+    //   // const [textContent] = questionObject[2 * i];
+    //   // questionDiv.textContent = textContent;
+    //   // questionDiv.textContent = questionData[2 * i][0];
+    // } else {
+    //   questionDiv.classList.add("question");
+    //   questionDiv.textContent = questionValues[i - 6];
+    // }
+    questionDiv.classList.add("questionTopic");
+    // use every second array to account for spreadsheet format
+    questionDiv.textContent = questionObject[`category${i}`].topic;
 
     gridArea.appendChild(questionDiv);
   }
