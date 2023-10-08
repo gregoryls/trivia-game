d720c3951dc533ed9ecf743ac17c1d3b539705b2
Greg Schneider <greg53x@msn.com>
2023-10-07 23:21:46 -0600

    remove custom addClass function

--- src/questionModal.js
+++ src/questionModal.js
@@ -14,10 +14,6 @@ export function removeClass(element, className) {
   element.classList.remove(className);
 }
 
-export function addClass(element, className) {
-  element.classList.add(className);
-}
-
 export function generateQuestionObjectFromCSV() {
   const questionObj = {};
 
