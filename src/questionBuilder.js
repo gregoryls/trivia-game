f9ff15a8d5a2792450978ea1eece1bab9ebb57f3
Greg Schneider <greg53x@msn.com>
2024-04-18 21:58:00 -0600

    function to create a label input pair

--- src/questionBuilder.js
+++ src/questionBuilder.js
@@ -1,1 +1,21 @@
 import "./style.css";
+
+function createLabelInput(inputID, type, labelText) {
+  const div = document.createElement("div");
+  const input = document.createElement("input");
+  const label = document.createElement("label");
+
+  input.id = inputID;
+  input.type = type;
+  label.htmlfor = inputID;
+  label.textContent = labelText;
+  div.append(label, input);
+  return div;
+}
+
+function renderCategoryInputs(categoryCount) {
+  const div = document.createElement("div");
+}
+document
+  .querySelector("body")
+  .append(createLabelInput("test", "text", "label"));
