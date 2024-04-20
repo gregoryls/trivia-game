d4d23ee9d9d6c6422863033f8257811781e5044c
Greg Schneider <greg53x@msn.com>
2024-04-19 19:30:59 -0600

    update question builder html title

--- src/questionBuilder.js
+++ src/questionBuilder.js
@@ -16,6 +16,13 @@ function createLabelInput(inputID, type, labelText) {
 function renderCategoryInputs(categoryCount) {
   const div = document.createElement("div");
 }
+
+const categoryNumberInput = document.getElementById("categoryNumberInput");
+
 document
   .querySelector("body")
   .append(createLabelInput("test", "text", "label"));
+
+categoryNumberInput.addEventListener("change", () => {
+  console.log(categoryNumberInput.value);
+});
